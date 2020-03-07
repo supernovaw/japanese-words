1f5e8bcb1b46ebc5ab5ef5fd7c4de80dd39bb36b
supernovaw <ruszrj@gmail.com>
2020-03-07 22:29:27 +1000

    Label: added font parameter and text change animation

--- src/gui/elements/Label.java
+++ src/gui/elements/Label.java
@@ -1,9 +1,6 @@
 package gui.elements;
 
-import gui.Bounds;
-import gui.Element;
-import gui.Scene;
-import gui.Theme;
+import gui.*;
 
 import java.awt.*;
 import java.awt.geom.Area;
@@ -12,37 +9,70 @@ public class Label extends Element {
 	private String text;
 	private int alignHorizontal;
 	private Area textArea;
+	private Font font;
+	// textChangeAnimate has object parameter Area (old textArea)
+	private OneWayAnimating textChangeAnimate;
 
-	public Label(String text, int alignHorizontal, Scene container, Bounds bounds) {
+	public Label(String text, int alignHorizontal, Font f, Scene container, Bounds bounds) {
 		super(container, bounds);
 		this.text = text;
 		this.alignHorizontal = alignHorizontal;
+		font = f.deriveFont(h() / 1.5f);
+		textChangeAnimate = new OneWayAnimating(500, this);
+	}
+
+	public Label(String text, Font f, Scene container, Bounds bounds) {
+		this(text, 0, f, container, bounds);
+	}
+
+	public Label(String text, int alignHorizontal, Scene container, Bounds bounds) {
+		this(text, alignHorizontal, Theme.getUIFont(), container, bounds);
 	}
 
 	public Label(String text, Scene container, Bounds bounds) {
-		super(container, bounds);
-		this.text = text;
+		this(text, Theme.getUIFont(), container, bounds);
 	}
 
-	public void setText(String text) {
+	public void changeText(String text) {
+		textChangeAnimate.animate(textArea);
+		textArea = null; // cause it to be updated on the next repaint
 		this.text = text;
-		repaint();
 	}
 
 	@Override
 	protected void paint(Graphics2D g) {
-		g.setFont(Theme.getUIFont());
-		g.setColor(Theme.getFG());
+		g.setFont(font);
 
 		if (textArea == null) {
 			double textX = alignStringX(g, text, 0, w(), alignHorizontal);
 			double textY = centerStringY(g, h() / 2);
 			textArea = getTextArea(text, textX, textY, g);
-
 		}
+
 		int tx = x(), ty = y();
 		g.translate(tx, ty);
-		g.fill(textArea);
+
+		double fadePhase = textChangeAnimate.getSine();
+		if (fadePhase == 1) { // just paint current text
+			g.setColor(Theme.getFG());
+			g.fill(textArea);
+		} else { // paint current and fading text
+			g.setColor(Theme.getFG(fadePhase));
+			g.fill(textArea);
+			g.setColor(Theme.getFG(1d - fadePhase));
+			g.fill((Area) textChangeAnimate.getParameter(0));
+		}
+
 		g.translate(-tx, -ty);
+	}
+
+	@Override
+	protected void onDisplay() {
+		textChangeAnimate.setDisplayed(true);
+	}
+
+	@Override
+	protected void onShut() {
+		textChangeAnimate.setDisplayed(false);
 	}
 }
--- src/gui/scenes/SceneExample.java
+++ src/gui/scenes/SceneExample.java
@@ -8,12 +8,22 @@ import gui.elements.*;
 public class SceneExample extends Scene {
 	public SceneExample(Window holder) {
 		super(holder);
-		addElement(new TextField("", "Type and edit text here", this, new Bounds(50, 70, 500, 40)));
-		addElement(new HorizontalRadioButtons(new String[]{"Option 1", "Option 2", "3", "4", "5"},
-				-1, this, new Bounds(50, 160, 500, 40)));
-		addElement(new Checkbox("Check me", this, new Bounds(50, 250, 150, 30)));
-		addElement(new Label("Read me", -1, this, new Bounds(50, 330, 200, 30)));
+		TextField tf = new TextField("", "Type and edit text here", this, new Bounds(50, 70, 500, 40));
+		Label l = new Label("Read me", -1, this, new Bounds(50, 330, 200, 30));
+		tf.setOnEnter(() -> l.changeText(tf.flushText()));
+		addElement(tf);
 
-		addElement(new Button("Return", () -> changeScene(sceneMain), this, new Bounds(50, 410, 125, 30)));
+		addElement(new HorizontalRadioButtons(new String[]{"Option 1", "Option 2", "3", "4", "5"}, -1,
+				this, new Bounds(50, 160, 500, 40)));
+
+		addElement(new Checkbox("Check me", this, new Bounds(50, 250, 150, 30)));
+		addElement(l);
+
+		RadioButtonGroup group = new RadioButtonGroup();
+		addElement(new RadioButton("First option", group, this, new Bounds(50, 410, 200, 30)));
+		addElement(new RadioButton("Second option", group, this, new Bounds(50, 450, 200, 30)));
+		addElement(new RadioButton("Third option", group, this, new Bounds(50, 490, 200, 30)));
+
+		addElement(new Button("Return", () -> changeScene(sceneMain), this, new Bounds(50, 570, 125, 30)));
 	}
 }
